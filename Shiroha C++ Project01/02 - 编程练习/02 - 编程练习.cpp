1dc0c59d55dbda94a1866d4e450c9931f58c03a2
Sora <627875862@qq.com>
2022-11-23 22:33:42 +0800

    practise

--- Shiroha C++ Project01/02 - 编程练习/02 - 编程练习.cpp
+++ Shiroha C++ Project01/02 - 编程练习/02 - 编程练习.cpp
@@ -4,11 +4,34 @@ using namespace std;
 
 int main()
 {
-	int age;
+	//int age;
 
-	cin >> age;
+	//cin >> age;
 
-	int months = age * 12;
+	//int months = age * 12;
 
-	cout << "你存在了" << months << "个月" << endl;
+	//cout << "你存在了" << months << "个月" << endl;
+
+	//cout << "米" << endl;
+
+	//float m;
+	//
+	//cin >> m;
+
+	//float cm = m * 100;
+
+	//cout << cm << "厘米" << endl;
+
+	cout << "秒" << endl;
+
+	int s;
+
+	cin >> s;	
+	
+	int day    = s / 86400;
+	int h      = (s - day * 86400) / 3600;
+	int min    = (s % 3600) / 60;
+	int s_Left = s % 60;
+
+	cout << day << "天" << h << "小时" << min << "分" << s_Left << "秒" << endl;
 }
